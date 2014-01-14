b20cafb5ecf6ad1802b4ff0902b0f4e789b75bf8
Kelly Heller <kkheller@cedrus.com>
2014-01-13 17:03:02 -0800

    re-writing this mach-o_dylib_fixer script from Bash to Python

--- macosx/cedrus_qt_mach-o_dylib_fixer.py
+++ macosx/cedrus_qt_mach-o_dylib_fixer.py
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env python
+
 
 # This script "fixes" the "install name id" and also the various Mach-O rel-paths of dylibs.
 
 # When we say "fix" we mean that absolute paths are changed to paths like @executable_path/libNAME.dylib
 
 # ARGUMENT ONE [required] -- the path to your target dir
-# ARGUMENT TWO [optional] -- set to anything you like; the mere presence of the second arg enables verbose output
 
 # This is designed to be applied to ONE DIRECTORY, at "one depth" of the directory only.
 # That means you should have some folder "folder_foo/" that basically contains
@@ -30,98 +30,92 @@
 #                  then after this script runs the dylib will report that it has been
 #                  linked against "@executable_path/libboost_filesystem-xgcc811-mt-1_37.dylib"
 
+import os, subprocess, time, sys, glob, shutil, datetime, platform
+
+def run_os_command_expecting_zero_output( command ):
+    the_stdout = os.popen( command ).read().rstrip()
+    if the_stdout != '':
+        print 'UNEXPECTED RESULTS:'
+        print the_stdout
 
 
-if [ -n "$1" ]
-then
+if len(sys.argv) < 2:
+    print '****************************** This script requires one argument. ******************************'
+else:
 
-        echo " --- mach-o_dylib_fixer.sh: BEGIN SCRIPT --- "
+    target_dir = sys.argv[1]
 
-        TARGET_DIR=$1
+    print " --- mach-o_dylib_fixer.sh: BEGIN SCRIPT --- "
 
-        if [ -n "$2" ]
-        then
-                VERBOSE="true"
-        fi
+    outer_glob = glob.glob( target_dir + '/*dylib' )
 
-        # shorten any dylib names like "*.5.1.1.dylib" so that they are simply:  *.5.dylib
-        for OUTER_LOOP_LIB_FILENAME in $TARGET_DIR/*.dylib
-        do
-                mv "$OUTER_LOOP_LIB_FILENAME" "${OUTER_LOOP_LIB_FILENAME/.5.1.1/.5}"
-        done # shortening the 5.1.1 names
+    outer_loop_dylibs_list = []
+    inner_loop_dylibs_list = []
 
-        for OUTER_LOOP_LIB_FILENAME in $TARGET_DIR/*.dylib
-        do
+    for dylib in outer_glob:
+        if False == os.path.islink( dylib ):
+            outer_loop_dylibs_list += [ dylib ]
+            inner_loop_dylibs_list += [ dylib ]
 
-            # strip off any path, even a path as small as "./"
-                OUTER_LOOP_LIB_FILENAME_BASENAME=`basename "$OUTER_LOOP_LIB_FILENAME"`
+    for outer_dylib in outer_loop_dylibs_list:
 
-                # find the current install name id of this dylib
-                OLD_ID_NAME=`otool -D "$OUTER_LOOP_LIB_FILENAME" | tail -1`
+        outer_file_basename = os.popen( 'basename "' + outer_dylib + '"' ).read().rstrip()
 
-                if [ -n "$VERBOSE" ]
-                then
-                        echo " --- Considering $OUTER_LOOP_LIB_FILENAME --- "
-                fi
+        old_id_name = os.popen( 'otool -D "' + outer_dylib + '" | tail -1'  ).read().rstrip()
 
+        desired_id = '@executable_path/' + outer_file_basename
 
-                # make sure the install name id is a "fixed" one
-                if [[ "$OLD_ID_NAME" != "$OUTER_LOOP_LIB_FILENAME_BASENAME" ]]
-                then
-                        echo "      --- Fixing install name id on $OUTER_LOOP_LIB_FILENAME_BASENAME --- "
+        if old_id_name != desired_id:
 
-                        SHORT_OUTER_LOOP_LIB_FILENAME=`echo ${OUTER_LOOP_LIB_FILENAME_BASENAME/.3.0.0/}`
-                        SHORT_OUTER_LOOP_LIB_FILENAME=`echo ${SHORT_OUTER_LOOP_LIB_FILENAME/.3/}`
-                        install_name_tool -id "@executable_path/$SHORT_OUTER_LOOP_LIB_FILENAME" "$OUTER_LOOP_LIB_FILENAME"
-                fi
+            print '      --- Fixing install name id on ' + outer_file_basename + ' --- '
 
+            command = 'install_name_tool -id "' + desired_id + '" "' + outer_dylib + '"'
+            run_os_command_expecting_zero_output( command )
 
-                # we used to do "part two" conditionally, but now just aggressively do it no matter what!
+        for inner_dylib in inner_loop_dylibs_list:
+            # PSEUDO-CODE FOR SECOND PORTION OF THIS ROUTINE:
+            #
+            #         For each dylib in this folder (i.e. "for each PEER of the outer-loop file"):
+            #         {
+            #                        If the list of linkages inside outer-loop-file CONTAINS a reference to peer:
+            #                        {
+            #                                Ensure that the linkage is a fixed reference.
+            #                        }
+            #         }
 
-                        # PSEUDO-CODE FOR SECOND PORTION OF THIS ROUTINE:
-                        #
-                        #         For each dylib in this folder (i.e. "for each PEER of the outer-loop file"):
-                        #         {
-                        #                        If the list of linkages inside outer-loop-file CONTAINS a reference to peer:
-                        #                        {
-                        #                                Ensure that the linkage is a fixed reference.
-                        #                        }
-                        #         }
-                        for INNER_LOOP_LIB_FILENAME in $TARGET_DIR/*.dylib
-                        do
-                                # strip off any path, even as small a path as "./"
-                                INNER_LOOP_LIB_FILENAME_BASENAME=`basename "$INNER_LOOP_LIB_FILENAME"`
+            inner_file_basename_01 = os.popen( 'basename "' + inner_dylib + '"' ).read().rstrip()
+            inner_file_basename_02 = '54321098760987654321'
 
-                                if [ -n "$VERBOSE" ]
-                                then
-                                        echo "      --- Checking for link to $INNER_LOOP_LIB_FILENAME_BASENAME in $OUTER_LOOP_LIB_FILENAME --- "
-                                fi
+            if inner_file_basename_01.endswith( '5.1.1.dylib' ):
+                inner_file_basename_02 = inner_file_basename_01.split('.')[0] + '.5.dylib'
 
+            print '      --- Checking for link to ' + inner_file_basename_01 + ' in ' + outer_file_basename + ' --- '
 
-                                # query for the presence of "peer" in the list of linkages (rel-paths) inside of the "outer-loop" dylib:
-                                FOUND_LINK=`otool -L "${OUTER_LOOP_LIB_FILENAME}" | grep   "${INNER_LOOP_LIB_FILENAME_BASENAME}" | grep version | awk '{print $1}'`
+            # query for the presence of "peer" in the list of linkages (rel-paths) inside of the "outer-loop" dylib:
+            command = 'otool -L "' + outer_dylib + '" | grep   "' + inner_file_basename_01 + '" | grep version | awk \'{print $1}\''
+            found_link = os.popen( command ).read().rstrip()
 
-                                # strip of any whitespace so that our "-n" test (for empty string) will work
-                                FOUND_LINK=`echo ${FOUND_LINK/ /}`
+            if found_link != '':
 
-                                # if the linkage is present in the list, then make sure it is "fixed"
-                                if [ -n "$FOUND_LINK" ]
-                                then
+                print '           --- Inside ' + outer_file_basename + ' found ' + found_link + '. (A) Proceeding to fix this Mach-O rel-path --- '
 
-                                                echo "           --- Inside $OUTER_LOOP_LIB_FILENAME_BASENAME found $FOUND_LINK. Proceeding to fix this Mach-O rel-path --- "
+                command =  str('install_name_tool -change "' + found_link +
+                               '" "@executable_path/' + inner_file_basename_01 + '" "' + outer_dylib + '"')
 
-                                                SHORT_INNER_LOOP_LIB_FILENAME=`echo ${INNER_LOOP_LIB_FILENAME_BASENAME/.3.0.0/}`
-                                                SHORT_INNER_LOOP_LIB_FILENAME=`echo ${SHORT_INNER_LOOP_LIB_FILENAME/.3/}`
+                run_os_command_expecting_zero_output( command )
 
-                                                install_name_tool -change "$FOUND_LINK" "@executable_path/$SHORT_INNER_LOOP_LIB_FILENAME" "$OUTER_LOOP_LIB_FILENAME"
-                                fi
-                        done # done with "Portion Two", the Inner Loop
+            # now query using 02 (inner_file_basename_02)
+            command = 'otool -L "' + outer_dylib + '" | grep   "' + inner_file_basename_02 + '" | grep version | awk \'{print $1}\''
+            found_link = os.popen( command ).read().rstrip()
 
-        done # done processing the input folder (folder given in argument one to this script)
+            if found_link != '':
 
-        echo " --- mach-o_dylib_fixer.sh: END SCRIPT --- "
+                print '           --- Inside ' + outer_file_basename + ' found ' + found_link + '. (B) Proceeding to fix this Mach-O rel-path --- '
 
-else
-        echo "****************************** This script requires one argument. ******************************"
-        false # terminates our script with an ERROR value
-fi
+                # IMPORTANT: we correct it to inner_file_basename_01 ( one! ) even though we found 02
+                command =  str('install_name_tool -change "' + found_link +
+                               '" "@executable_path/' + inner_file_basename_01 + '" "' + outer_dylib + '"')
+
+                run_os_command_expecting_zero_output( command )
+
+    print " --- mach-o_dylib_fixer.sh: END SCRIPT --- "
